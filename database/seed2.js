c370b7c83f2951874a9390b9f07cb9135b30f4fe
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-07 16:57:17 -0700

    Updated the dummy data

--- database/seed2.js
+++ database/seed2.js
@@ -35,61 +35,75 @@ var images = generateImage();
 var names = generateName();
 var sentences = generateSentence();
 var colors = generateColor();
+let sizes = [
+  'X-Small', 'Small', 'Medium', 'Large', 'X-Large',
+  'Small', 'Medium', 'X-Large',
+  'X-Small', 'Medium', 'Large',
+  'Small', 'Medium', 'Large', 'X-Large',
+  'X-Small', 'X-Large'
+]
+let designers = ['Gucci', 'Givency', 'Burberry', 'ALice + Olivia', 'Alexander Wang', 'Max Mara', 'Michael Kors', 'Moschino', 'Theory', 'Thom Browne']
+var itemType = ['shoes','dress','earings','bags','bracelet','purses','dress','suit','shirt','t-shirt','jacket','pants',
+'watch','small bags','tie','socks','towel']
+let counter = 1;
 const generateData = () => {
   const random1 = Math.floor(Math.random() * Math.floor(images.length))
   const random2 = Math.floor(Math.random() * Math.floor(images.length))
   const random3 = Math.floor(Math.random() * Math.floor(images.length));
+  const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
+  const randomDesigner = Math.floor(Math.random() * Math.floor(designers.length));
+  const randomSize =  Math.floor(Math.random() * Math.floor(sizes.length));
   const productname = names[random1];
   const imageurl = [images[random1], images[random2], images[random3]];
+  let clothingsize = [];
+  for(var i = 0; i< randomSize; i++){
+    clothingsize.push(sizes[i])
+  }
 
   const shoesizes = [random1, random2, random3];
-  const designer = names[random1];
+  const designer = designers[randomDesigner];
   const price = Math.floor(Math.random() * Math.floor(500));
   const color = colors[random1]
-  const itemType = names[random2];
+  const itemtype = itemType[randomItem]
   const description = sentences[random1];
   const data = {
+    id: counter++,
     productname: productname,
     imageurl: imageurl,
     shoesizes: shoesizes,
     designer: designer,
+    clothingsizes: clothingsize,
     price: price,
     color: color,
-    itemType: itemType,
+    itemtype: itemtype,
     description: description
   }
   return data;
 }
-let dummyData = [];
-for (var i = 0; i < 500000; i++) {
-  dummyData.push(generateData())
-}
 
-
-var str = JSON.stringify(dummyData, null, 2);
-var data = str.slice(1, str.length - 1) + ','
 
 let writeStream = fs.createWriteStream('../dummydata.json', { flags: 'w' });
-function writeOneMillionTimes(writer, data, encoding, callback) {
-  let i = 12;
+function writeOneMillionTimes(writer, encoding, callback) {
+  let i = 10000002;
   write();
   function write() {
     let ok = true;
+    
     do {
-      console.log(ok)
       i--;
-      if (i === 11) {
+      if (i === 10000001) {
         writer.write('[', encoding, callback);
       } else if (i === 1) {
-        writer.write(data.slice(0, data.length - 1), encoding, callback);
+        let randomData = JSON.stringify( generateData(), null, 2)
+        writer.write(randomData, encoding, callback);
       } else if (i === 0) {
         // last time!
         writer.write(']', encoding, callback);
       } else {
         // See if we should continue, or wait.
         // Don't pass the callback, because we're not done yet.
-        ok = writer.write(data, encoding);
-        console.log(ok);
+        let randomData = JSON.stringify( generateData(), null, 2) + ',';
+        ok = writer.write(randomData, encoding);
       }
     } while (i > 0 && ok);
     if (i > 0) {
@@ -101,7 +115,7 @@ function writeOneMillionTimes(writer, data, encoding, callback) {
   }
 }
 
-writeOneMillionTimes(writeStream, data, 'utf8', (err) => {
+writeOneMillionTimes(writeStream, 'utf8', (err) => {
   if (err) {
     console.log('Error', err)
   } else {
