6da286e51f41b388ce8818cee9a249de1a52deac
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-07 16:59:33 -0700

    Created router for get request

--- server/routes.js
+++ server/routes.js
@@ -2,23 +2,26 @@ const express = require('express');
 const controller = require('./controller.js');
 const router = express.Router();
 
+router
+  .route('/api')
+  .get(controller.get)
 
-router
-  .route('/api/purses')
-  .get(controller.getpurses)
+// router
+//   .route('/api/purses')
+//   .get(controller.getpurses)
 
-router
-  .route('/api/shoes')
-  .get(controller.getshoes)
+// router
+//   .route('/api/shoes')
+//   .get(controller.getshoes)
 
-router
-  .route('/api/earrings')
-  .get(controller.getearrings)
-router
-  .route('/api/bracelets')
-  .get(controller.getbracelets)
-router
-  .route('/api/dress')
-  .get(controller.getdresses)
+// router
+//   .route('/api/earrings')
+//   .get(controller.getearrings)
+// router
+//   .route('/api/bracelets')
+//   .get(controller.getbracelets)
+// router
+//   .route('/api/dress')
+//   .get(controller.getdresses)
 
 module.exports = router;
