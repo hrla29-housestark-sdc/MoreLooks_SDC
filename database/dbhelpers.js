d9ec14a34a6fad08761ea7bb58c394fd68ea63f0
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-07 16:56:50 -0700

    Testing look up times

--- database/dbhelpers.js
+++ database/dbhelpers.js
@@ -1,13 +1,12 @@
 const Product = require('./index.js');
-
-const random = (data) => {
-  return Math.floor(Math.random() * data.length)
-}
-
+var itemType = ['shoes','dress','earings','bags','bracelet','purses','dress','suit','shirt','t-shirt','jacket','pants',
+'watch','small bags','tie','socks','towel']
 const dbhelpers = {
-  get: (querystring) => {
-    return Product.find({itemtype: querystring})
-      .then(data => data[random(data)])
+  
+  get: () => {
+    const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
+    return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]},{id:1, itemtype:1}).limit(5).explain('executionStats')
+      .then(data => data)
       .catch(err => console.error(err))
   },
 }
