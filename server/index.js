a474775164b837be1aab011fde1f8c60a4e894cc
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-07 16:58:19 -0700

    Updated port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const parser = require('body-parser');
 const router = require('./routes.js');
 
 const server = express();
-const port = 3003;
+const port = 1002;
 
 server.use(morgan('dev'))
 server.use(parser.json())
