ff4313db49452c33683d6f898b15f3fa7e8e852b
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-18 15:41:16 -0700

    Refactored the helper

--- database/dbhelpers.js
+++ database/dbhelpers.js
@@ -7,25 +7,7 @@ const dbhelpers = {
   get: () => {
     const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
     return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]}).limit(1)
-      // .then(data => data)
-      // .catch(err => console.error(err))
   }
-
-  // get: () => {
-  //   const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
-  //   return redis.get(itemType)
-  //     .then((reply) => {
-  //       if(reply){
-  //         return JSON.parse(reply)
-  //       }else {
-
-  //       }
-  //     })
-  //     .catch(err => console.log(err))
-  //   return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]}).limit(1).explain('executionStats')
-  //     .then(data => data)
-  //     .catch(err => console.error(err))
-  // }
  
 }
 module.exports = dbhelpers;
