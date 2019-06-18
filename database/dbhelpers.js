1f0c8763a3e1e08d850505adf87eb27d174c9530
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-17 19:07:46 -0700

    Update get helper

--- database/dbhelpers.js
+++ database/dbhelpers.js
@@ -1,16 +1,31 @@
 const Product = require('./index.js');
+// const redis = require('../server/index.js')
 var itemType = ['shoes','dress','earings','bags','bracelet','purses','dress','suit','shirt','t-shirt','jacket','pants',
 'watch','small bags','tie','socks','towel']
 const dbhelpers = {
   
   get: () => {
     const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
-    return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]}).limit(1).explain('executionStats')
-      .then(data => data)
-      .catch(err => console.error(err))
-  },
-  post: () => {
-
+    return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]}).limit(1)
+      // .then(data => data)
+      // .catch(err => console.error(err))
   }
+
+  // get: () => {
+  //   const randomItem = Math.floor(Math.random() * Math.floor(itemType.length));
+  //   return redis.get(itemType)
+  //     .then((reply) => {
+  //       if(reply){
+  //         return JSON.parse(reply)
+  //       }else {
+
+  //       }
+  //     })
+  //     .catch(err => console.log(err))
+  //   return Product.find({id: { $gte: 9000000, $lte: 10000000 } ,itemtype: itemType[randomItem]}).limit(1).explain('executionStats')
+  //     .then(data => data)
+  //     .catch(err => console.error(err))
+  // }
+ 
 }
 module.exports = dbhelpers;
